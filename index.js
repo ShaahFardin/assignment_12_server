c7ef489c8807add6b0cae7e8a47234d40c967860
shaahfardin <fardinshaah@gmail.com>
2022-11-24 01:36:14 +0600

    Monogodb database connected

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,7 +9,25 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res)=>{
-   res.send("Final project carvana server is running");
+
+const uri = process.env.REACT_APP_MONGO_URI;
+const client = new MongoClient(uri,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverApi: ServerApiVersion.v1
+    });
+
+async function run(){
+    try {
+        console.log('MongoDb Database connected');
+    } finally {
+        
+    }
+}
+run().catch(error=>console.log(error))
+
+app.get('/', (req, res) => {
+    res.send("Final project carvana server is running");
 });
 app.listen(port, console.log(`Carvana server is running on port ${port}`))
