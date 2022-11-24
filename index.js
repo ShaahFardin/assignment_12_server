1948a38b8e05f4555e888f0d6ecd2af2d803c36b
shaahfardin <fardinshaah@gmail.com>
2022-11-24 21:05:48 +0600

    Role added for user

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
@@ -18,14 +19,31 @@ const client = new MongoClient(uri,
         serverApi: ServerApiVersion.v1
     });
 
-async function run(){
+async function run() {
     try {
+
+        const usersCollecton = client.db('carvana').collection('users');
+
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: user,
+            }
+            const result = await usersCollecton.updateOne(filter, updatedDoc, options)
+            console.log(result);
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+            res.send({result, token})
+        })
         console.log('MongoDb Database connected');
+
     } finally {
-        
+
     }
 }
-run().catch(error=>console.log(error))
+run().catch(error => console.log(error))
 
 app.get('/', (req, res) => {
     res.send("Final project carvana server is running");
